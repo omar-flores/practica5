fdc8da6c0c73a4bc675dd6232719ca0752f9a6c5
omar flores <reydek-14@hotmail.com>
2014-08-07 08:58:16 -0500

    piano
    teclas con sonido de piano

---
+++ js/acciones.js
@@ -0,0 +1,28 @@
+//JavaScript
+	var audio
+$(document).ready(function(e){
+	$ ("#principal").height ($("#page").height())  ;
+	document.addEventListener("deviceready",function(){
+audio=window.plugins.lowlatencyAudio;
+audio.preoladFX('do','audio/C.mp3',function (msg) {},
+function(msg){alert ('error: '+ msg);});
+audio.preoladFX('re','audio/D.mp3',function (msg) {},
+function(msg){alert ('error: '+ msg);});
+audio.preoladFX('mi','audio/E.mp3',function (msg) {},
+function(msg){alert ('error: '+ msg);});
+audio.preoladFX('fa','audio/F.mp3',function (msg) {},
+function(msg){alert ('error: '+ msg);});
+audio.preoladFX('sol','audio/G.mp3',function (msg) {},
+function(msg){alert ('error: '+ msg);});
+audio.preoladFX('la','audio/A.mp3',function (msg) {},
+function(msg){alert ('error: '+ msg);});
+audio.preoladFX('si','audio/B.mp3',function (msg) {},
+function(msg){alert ('error: '+ msg);});
+
+
+
+
+
+
+ },false); //deviceready
+});//ready
