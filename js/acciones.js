ffd29b30876e6922adcc50a058e8dfcb2fffa9b2
omar-flores <reydek-14@hotmail.com>
2014-08-08 08:23:19 -0500

    CORRECIONES
    CORRECIONES ORTOGRAFICAS

--- js/acciones.js
+++ js/acciones.js
@@ -1,34 +1,25 @@
 //JavaScript
-	var audio
+	var audio;
 $(document).ready(function(e){
-	$ ("#principal").height ($("#page").height())  ;
+	$('#Principal').height($('#page').height());
 	document.addEventListener("deviceready",function(){
-audio=window.plugins.lowlatencyAudio;
-audio.preoladFX('do','audio/C.mp3',function (msg) {},
-function(msg){alert ('error: '+ msg);});
-audio.preoladFX('re','audio/D.mp3',function (msg) {},
-function(msg){alert ('error: '+ msg);});
-audio.preoladFX('mi','audio/E.mp3',function (msg) {},
-function(msg){alert ('error: '+ msg);});
-audio.preoladFX('fa','audio/F.mp3',function (msg) {},
-function(msg){alert ('error: '+ msg);});
-audio.preoladFX('sol','audio/G.mp3',function (msg) {},
-function(msg){alert ('error: '+ msg);});
-audio.preoladFX('la','audio/A.mp3',function (msg) {},
-function(msg){alert ('error: '+ msg);});
-audio.preoladFX('si','audio/B.mp3',function (msg) {},
-function(msg){alert ('error: '+ msg);});
-$ ('.nota').bind('touchstart',function (e) {
-	$(this).addClass('tocada');
-	audio.play($(this).attr('id'));
-}).bind('touched',function(e){
-	$(this).removeClass('tocada');
-});
-
-
-
-
-
-
+    audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('do','audio/C.mp3',function(msg){},function(msg){alert('error: '+msg);});
+	audio.preloadFX('re','audio/D.mp3',function(msg){},function(msg){alert('error: '+msg);});
+	audio.preloadFX('mi','audio/E.mp3',function(msg){},function(msg){alert('error: '+msg);});
+	audio.preloadFX('fa','audio/F.mp3',function(msg){},function(msg){alert('error: '+msg);});
+	audio.preloadFX('sol','audio/G.mp3',function(msg){},function(msg){alert('error: '+msg);});
+	audio.preloadFX('la','audio/A.mp3',function(msg){},function(msg){alert('error: '+msg);});
+	audio.preloadFX('si','audio/B.mp3',function(msg){},function(msg){alert('error: '+msg);});
+	
+	$('.nota').bind('touchstart',function(e){
+		$(this).addClass('tocada');
+		audio.play($(this).attr('id'));
+	}).bind('touchend',function(e){
+		$(this).removeClass('tocada');
+	});//device
+	
+	
+	
  },false); //deviceready
 });//ready
